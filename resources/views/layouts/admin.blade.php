b3ef66964b01523dba7f6f40c1106948c3626715
rifatfarabi <rifatfarabi08@gmail.com>
2023-06-08 09:25:52 +0600

    icon added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,7 +48,7 @@
                                 Dashboard
                             </a>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-cart-shopping"></i></div>
                                 Products
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
